6881597838015f6899f7f15a62a9d0d4ae981ee9
Daniel <danirvicente@gmail.com>
2022-08-23 18:08:11 +0200

    fetching data and input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import axios from "axios"
 
 function App() {
     const apiURL = "https://restcountries.com/v3.1/all"
+    const [countries, setCountries] = useState([])
+    const [name, setName] = useState("")
+
+    useEffect(() => {
+        axios.get(apiURL).then((response) => {
+            setCountries(response.data.map((country) => country.name.common))
+        })
+    }, [])
+
+    const handleChange = (event) => setName(event.target.value)
+
+    return (
+        <>
+            <p>
+                find countries
+                <input value={name} onChange={handleChange} />
+            </p>
+        </>
+    )
 }
 
 export default App
