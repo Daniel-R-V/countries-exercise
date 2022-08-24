8101d802ea52141bd666dfffb12f16c21afca13b
Daniel <danirvicente@gmail.com>
2022-08-24 17:43:01 +0200

    filter countries and show button

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
 
     useEffect(() => {
         axios.get(apiURL).then((response) => {
-            setCountries(response.data.map((country) => country.name.common))
+            setCountries(response.data)
         })
     }, [])
 
     const handleChange = (event) => setName(event.target.value)
 
-    const filterCountries = countries.filter((country) => country.toLowerCase().includes(name))
+    const filterCountries = countries.filter((country) => country.name.common.toLowerCase().includes(name))
 
     return (
         <>
@@ -22,9 +22,46 @@ function App() {
                 find countries
                 <input value={name} onChange={handleChange} />
             </p>
-            {filterCountries.length > 10 ? "too many matches" : filterCountries.map((country) => <p key={country}>{country}</p>)}
+            <FilterResponse filterCountries={filterCountries} setName={setName} />
         </>
     )
+}
+
+const FilterResponse = ({ filterCountries, setName }) => {
+    if (filterCountries.length === 1) {
+        const country = filterCountries[0]
+        return (
+            <div>
+                <h1>{country.name.common}</h1>
+
+                <p>capital {country.capital}</p>
+                <p>population {country.population}</p>
+
+                <h1>languages</h1>
+
+                <ul>
+                    {Object.values(country.languages).map((language) => (
+                        <li key={language}>{language}</li>
+                    ))}
+                </ul>
+                <div>
+                    <img src={country.flags.png} alt={country.name} />
+                </div>
+            </div>
+        )
+    }
+
+    if (filterCountries.length > 10) return <div>Too many matches, specify another filter</div>
+    return filterCountries.map((country) => {
+        return (
+            <div key={country.name.common}>
+                {country.name.common}
+                <button value={country.name.common.toLowerCase()} onClick={(event) => setName(event.target.value)}>
+                    show
+                </button>
+            </div>
+        )
+    })
 }
 
 export default App
